92ebb9f1d53cc238c8d915838fe11c38db116598
user52dev <user52dev@gmail.com>
2024-09-03 16:49:04 +0300

    added search and filters page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,40 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
-        <div className="bg-white rounded-lg shadow-md py-8 flex-col gap-5 text-center -mt-16">
-            <h1 className="text-5xl font-bold tracking-tight text-blue-600">
-                Tuck into a takeway today
-            </h1>
-            <span className="text-xl">Food is just a click away!</span>
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-blue-600">
+          Tuck into a takeway today
+        </h1>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar searchQuery="" placeHolder="Only Ioannina for now" onSubmit={handleSearchSubmit} />
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeawy even faster!
+          </span>
+          <span>
+            Download the Pamvotis app for faster ordering and personalised
+            recommendations
+          </span>
+          <img src={appDownloadImage} />
         </div>
-        <div className="grid md:grid-cols-2 gap-5">
-            <img src={landingImage} />
-            <div className="flex flex-col items-center justify-center gap-4 text-center">
-                <span className="font-bold text-3xl tracking-tighter">
-                    Order takeawy even faster!
-                </span>
-                <span>
-                    Download the Pamvotis app for faster ordering and personalised recommendations
-                </span>
-                <img src={appDownloadImage}/>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
